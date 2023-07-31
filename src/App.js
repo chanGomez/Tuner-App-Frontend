20961379c954f17c09a9203948b6c9d91692bf2c
Chantal <chantalgomez@pursuit.org>
2023-07-30 22:27:25 -0400

    One feature missing/ almost completely done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home/Home";
+import Songs from './components/Songs/Songs';
+import NewSong from './components/NewSong/NewSong';
+import Song from './components/Song/Song';
+import EditSong from './components/EditSong/EditSong'
+import Playlists from './components/Playlists/Playlists'
+import Playlist from './components/Playlists/Playlist'
+import EditPlaylist from './components/Playlists/EditPlaylists'
+import NewPlaylist from './components/NewPlaylist/NewPlaylist'
+import Nav from './components/Nav/Nav';
+import './App.css'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav></Nav>
+        <Routes> 
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/songs" element={<Songs/>}></Route>
+          <Route path="/songs/:id" element={<Song/>}></Route>
+          <Route path="/songs/new" element={<NewSong/>}></Route>
+          <Route path="/songs/:id/edit" element={<EditSong/>} />
+          <Route path="/playlists" element={<Playlists/>} />
+          <Route path="/playlists/new" element={<NewPlaylist/>} />
+          <Route path="/playlists/:id" element={<Playlist/>} />
+          <Route path="/playlists/:id/edit" element={<EditPlaylist/>} />
+          <Route path="/404" element={<h1>404 Not found!</h1>} />
+          <Route path="*" element={<h1>404 Not found!</h1>} /> */
+        </Routes>
+      </Router>
     </div>
   );
 }
